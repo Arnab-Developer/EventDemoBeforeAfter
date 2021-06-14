05e0f1a49a9126087c72c27394d5ab0375236cb3
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-14 11:03:45 +0530

    Using static

--- EventDemoBeforeAfter/Program.cs
+++ EventDemoBeforeAfter/Program.cs
@@ -1,32 +1,32 @@
 ﻿using EventDemoBeforeAfter;
-using System;
+using static System.Console;
 
 Student s1 = new() { Name = "jon", Age = 10 };
-Console.WriteLine($"s1: Name {s1.Name} Age {s1.Age}");
-s1.StudentAgeChanging += e => Console.WriteLine($"Age changing. Old age: {e.OldAge}, New age: {e.NewAge}");
-s1.StudentAgeChanged += e => Console.WriteLine($"Age changed. Old age: {e.OldAge}, New age: {e.NewAge}");
+WriteLine($"s1: Name {s1.Name} Age {s1.Age}");
+s1.StudentAgeChanging += e => WriteLine($"Age changing. Old age: {e.OldAge}, New age: {e.NewAge}");
+s1.StudentAgeChanged += e => WriteLine($"Age changed. Old age: {e.OldAge}, New age: {e.NewAge}");
 s1.Age++;
-Console.WriteLine($"s1: Name {s1.Name} Age {s1.Age}");
+WriteLine($"s1: Name {s1.Name} Age {s1.Age}");
 
-Console.WriteLine();
+WriteLine();
 
-Console.Write("Do you want to cancel(Y/N): ");
-char input = Console.ReadKey().KeyChar;
+Write("Do you want to cancel(Y/N): ");
+char input = ReadKey().KeyChar;
 bool isCancel = input == 'Y' || input == 'y';
 
-Console.WriteLine();
-Console.WriteLine();
+WriteLine();
+WriteLine();
 
 Student s2 = new() { Name = "bob", Age = 20 };
-Console.WriteLine($"s2: Name {s2.Name} Age {s2.Age}");
+WriteLine($"s2: Name {s2.Name} Age {s2.Age}");
 s2.StudentAgeChanging += e =>
 {
-    Console.WriteLine($"Age changing. Old age: {e.OldAge}, New age: {e.NewAge}");
+    WriteLine($"Age changing. Old age: {e.OldAge}, New age: {e.NewAge}");
     e.IsCancel = isCancel;
 };
-s2.StudentAgeChanged += e => Console.WriteLine($"Age changed. Old age: {e.OldAge}, New age: {e.NewAge}");
+s2.StudentAgeChanged += e => WriteLine($"Age changed. Old age: {e.OldAge}, New age: {e.NewAge}");
 s2.Age = 45;
-Console.WriteLine($"s2: Name {s2.Name} Age {s2.Age}");
+WriteLine($"s2: Name {s2.Name} Age {s2.Age}");
 
 /*
 Output:
